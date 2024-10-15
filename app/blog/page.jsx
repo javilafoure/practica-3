13f65a719dfa460d64c4bbe9700770fe20ba2122
Joseph Antony Pierre Avila Foure - Tismart <extjpierre@belcorp.biz>
2024-10-14 20:27:01 -0500

    Add navigation

---
+++ app/blog/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Blog() {
+  return (
+    <div>
+      <h2>Page Blog</h2>
+    </div>
+  )
+}
+
+export default Blog
---
+++ app/home/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../components/NavBar/page'
+
+function Home() {
+  return (
+    <div>
+      <NavBar/>
+      <h2>Home</h2>
+    </div>
+  )
+}
+
+export default Home
