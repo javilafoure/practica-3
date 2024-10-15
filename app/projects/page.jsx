669a35da90c12681bb4cd5264a5283129c483d33
Joseph Antony Pierre Avila Foure - Tismart <extjpierre@belcorp.biz>
2024-10-15 01:50:39 -0500

    add detalles

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -6,7 +6,7 @@ function Projects() {
     <div className='flex flex-col justify-center'>
       <NavBar />
       <h2 className='flex justify-center font-bold text-3xl py-8 text-black'>Our Projects</h2>
-      <p className='text-black flex justify-center text-center'>A project is a single or team activity, combining research and design with best<br/> effort. Here are some of our team's projects</p>
+      <p className='text-black flex justify-center text-center'>A project is a single or team activity, combining research and design with best<br/> effort. Here are some of our teams projects</p>
       <div className='flex justify-center py-14'>
         <div className='flex justify-center border rounded-lg shadow p-3'>
 
