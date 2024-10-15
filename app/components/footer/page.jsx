8c6ff590020ef5b56b781be51c200c47a4ce2a19
Joseph Antony Pierre Avila Foure - Tismart <extjpierre@belcorp.biz>
2024-10-14 23:24:58 -0500

    add footer

---
+++ app/components/footer/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='flex align-bottom w-full h-48 border rounded-lg shadow'></footer>
+  )
+}
+
+export default Footer
--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import NavBar from '../components/NavBar/page'
+import Footer from '../components/footer/page'
 
 function Main() {
   return (
-    <div>
-      <NavBar/>
-      <h2 className='text-black'>home</h2>
+    <div className='flex flex-col justify-center h-screen'>
+      <NavBar />
+
+      <div className='flex flex-col py-14 flex-grow'>
+        <div className='flex justify-center px-3 gap-8'>
+          <div className='w-96 h-80 border rounded-lg shadow'></div>
+          <div className='w-96 h-80 border rounded-lg shadow'></div>
+
+        </div>
+
+      </div>
+      <Footer />
     </div>
   )
 }
